ea1f35bb1a13311a4ce451825617ab67ec1c2387
sewmini  010 <SewminiKangara@NIBMADMIN03s-iMac.local>
2025-05-16 11:10:09 +0530

    First commit

---
+++ Squregame/Squregame/SquregameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquregameApp.swift
+//  Squregame
+//
+//  Created by sewmini  010   on 2025-05-04.
+//
+
+import SwiftUI
+
+@main
+struct SquregameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Squregame/SquregameTests/SquregameTests.swift
@@ -0,0 +1,17 @@
+//
+//  SquregameTests.swift
+//  SquregameTests
+//
+//  Created by sewmini  010   on 2025-05-04.
+//
+
+import Testing
+@testable import Squregame
+
+struct SquregameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
