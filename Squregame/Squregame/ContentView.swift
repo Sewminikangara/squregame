122c963e8a3d502e6e4c462f71ad6862f9de8af5
sewmini  010 <SewminiKangara@NIBMADMIN03s-iMac.local>
2025-05-16 11:10:09 +0530

    First commit

---
+++ Squregame/Squregame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Squregame
+//
+//  Created by sewmini  010   on 2025-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
